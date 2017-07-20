5b2f8c7c38bd4cf0a5a67e3d0849cfe7e24c6c73
jundong <linjundong@hotmail.com>
2017-07-20 16:09:19 -0500

    Adding membership DAO and pointcut to account DAO

--- src/com/aop/MainDemoApp.java
+++ src/com/aop/MainDemoApp.java
@@ -3,15 +3,23 @@ package com.aop;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import com.aop.dao.AccountDAO;
+import com.aop.dao.MembershipDAO;
 
 public class MainDemoApp {
 
 	public static void main(String[] args){
 		AnnotationConfigApplicationContext context= 
 				new AnnotationConfigApplicationContext(DemoConfig.class);
-	AccountDAO theAccountDAO = context.getBean("accountDAO", AccountDAO.class);
+
+		AccountDAO theAccountDAO = context.getBean("accountDAO", AccountDAO.class);
 	
 	theAccountDAO.addAccount();
+	
+	//adding account with different DAO membership
+	
+	MembershipDAO theMemDAO = context.getBean("membershipDAO", MembershipDAO.class);
+	
+	theMemDAO.addAccount();
 	
 	context.close();
 
--- src/com/aop/aspect/LoggingAspect.java
+++ src/com/aop/aspect/LoggingAspect.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class LoggingAspect {
 
-	@Before("execution(public void addAccount())")
+	@Before("execution(public void com.aop.dao.AccountDAO.addAccount())")
 	public void beforeAddAccount(){
 		System.out.println("\n =====> executing @Before advice\n");
 		
